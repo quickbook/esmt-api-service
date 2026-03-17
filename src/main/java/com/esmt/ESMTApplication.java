5f632625ab16268f7a7e31d166a41b5415fe7e0e
Palam Brahma Reddy <166218892+brahma2242p@users.noreply.github.com>
2026-03-17 10:39:41 +0530

    Update ESMTApplication.java

--- src/main/java/com/esmt/ESMTApplication.java
+++ src/main/java/com/esmt/ESMTApplication.java
@@ -3,8 +3,10 @@ package com.esmt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
+@EnableCaching
 public class ESMTApplication {
 
 	public static void main(String[] args) {
